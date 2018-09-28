5198d9ebf8b7ea238676f5521dab134e9c988062
student <student@L411-S30>
2018-09-28 11:59:07 +0800

    Hello World to Hello Jom!

--- src/javaapplication2/JavaApplication2.java
+++ src/javaapplication2/JavaApplication2.java
@@ -6,20 +6,37 @@
 package javaapplication2;
 
 /**
- *
+ * @
  * @author student
  */
 public class JavaApplication2 {
-
-
-
+    
+    /**
+    *This method doesn't return anything
+    *Print Hello world
+    **/
+    
     public static void Hello() {
         System.out.println("Hello Jom!");
 
     }
-
+    
+    /** 
+    * This method returns the sum of 2 numbers
+    * @param x accepts the first input
+    * @param y accepts the second input
+    * @return sum - the sum of the two input
+    **/
+    
+    public static int sum(int x, int y) {
+        int sum = x + y;
+        
+        return sum;
+    }
+    
     public static void main(String[] args) {
         Hello();
+        sum(4,2);
     }
 
 }
